a50d256523d5bd6826b504b169dc4f9e2ba1d885
Ch-byte-afk <christianmullooly@proton.me>
2025-09-04 16:32:37 -0400

    'test'

--- src/Server.cpp
+++ src/Server.cpp
@@ -40,7 +40,7 @@ bool matchGroup(const std::string& input_line, const std::vector<Expression>::it
 	
 	if(!inverted){
 		while(((*(currExp + index)).type != Expression::GROUP_END) && ((*(currExp + index)).type != Expression::END_OF_FILE)){
-			std::vector<Expression> subVec = {currExp + index, Expression("\0")};
+			std::vector<Expression> subVec = {*(currExp + index), Expression("\0")};
 			if(matchHere(input_line, subVec.begin())){
 				return 1;
 			}
@@ -49,7 +49,7 @@ bool matchGroup(const std::string& input_line, const std::vector<Expression>::it
 		
 	} else {
 		while(((*(currExp + index)).type != Expression::GROUP_END) && ((*(currExp + index)).type != Expression::END_OF_FILE)){
-			std::vector<Expression> subVec = {currExp + index, Expression("\0")};
+			std::vector<Expression> subVec = {*(currExp + index), Expression("\0")};
 			if(!matchHere(input_line, subVec.begin()) && input_line != ""){
 				return 1;
 			}
