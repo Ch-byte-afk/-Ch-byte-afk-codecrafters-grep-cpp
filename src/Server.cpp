86b2aa484aedae8be167f6e0db36e78e34e46271
Ch-byte-afk <christianmullooly@proton.me>
2025-09-04 23:53:29 -0400

    'test'

--- src/Server.cpp
+++ src/Server.cpp
@@ -116,7 +116,7 @@ bool matchEitherOr(const std::string& input_line, const std::vector<Expression>:
 		std::cout << std::endl;
 	
 		if (matchHere(input_line, subScope.begin())){
-			return 1;
+			return matchHere(input_line.substr(subScope.size()), scope.end());
 		}
 	}
 	
