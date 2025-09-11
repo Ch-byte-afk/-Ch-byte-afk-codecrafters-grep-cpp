a967bcb1ee0fc6b27ddea5fa095dc9535778c1d6
Ch-byte-afk <christianmullooly@proton.me>
2025-09-11 01:15:40 -0400

    'test'

--- src/regEx.cpp
+++ src/regEx.cpp
@@ -6,7 +6,7 @@
 #include <iostream>
 
 
-Expression::Expression(std::string expString) : typeString(expString) {
+regEx::Expression::Expression(std::string expString) : typeString(expString) {
 	
 	switch(expString[0]){
 	
@@ -33,23 +33,15 @@ Expression::Expression(std::string expString) : typeString(expString) {
 			return;
 		
 		case '[':
-			type = GROUP_START;
-			return;
-			
-		case ']':
-			type = GROUP_END;
+			type = ANY_OF;
 			return;
 			
 		case '(':
-			type = EITHER_OR_START;
+			type = SCOPE;
 			return;
 			
 		case '|':
-			type = EITHER_OR_MIDDLE;
-			return;
-		
-		case ')':
-			type = EITHER_OR_END;
+			type = OR;
 			return;
 		
 		case '^':
@@ -72,10 +64,6 @@ Expression::Expression(std::string expString) : typeString(expString) {
 			type = MATCH_ZERO_OR_MORE;
 			return;
 			
-		case '\0':
-			type = END_OF_FILE;
-			return;
-		
 		default:
 			type = EXACT;
 			return;
@@ -83,19 +71,303 @@ Expression::Expression(std::string expString) : typeString(expString) {
 	}
 }
 
-
-std::vector<Expression> parsePattern(const std::string& pattern){
+std::vector<regEx::Expression> regEx::parsePattern(const std::string& pattern){
 	std::vector<Expression> expVec;
 	
+	std::string::const_iterator scopeStart = pattern.begin();
+	std::string::const_iterator scopeEnd; // "Off the end" iterator for sub-strings.
 	
-	for(int index = 0; index <= pattern.size(); index++){
-		if(pattern[index] == '\\'){
-			expVec.push_back(Expression(pattern.substr(index, 2)));
-			index++;
-		} else {
-			expVec.push_back(Expression(pattern.substr(index, 1)));
-		}
+	
+	while(scopeStart != pattern.cend()){
+		switch(*scopeStart){
+			
+			case '\\':
+			
+				if(scopeStart + 1 == pattern.cend()){
+					throw std::runtime_error("Missing argument for expression \"\\\": " + pattern);
+					return expVec;
+				}
+				
+				scopeEnd = scopeStart + 2;
+				
+				expVec.push_back(Expression(std::string(scopeStart, scopeEnd)));
+				scopeStart = scopeEnd;
+				break;
+			
+			case '[':
+				
+				scopeEnd = scopeStart + 1;
+				
+				if (findScope(scopeStart, scopeEnd, pattern, '[', ']')){
+						expVec.push_back(Expression(std::string(scopeStart, scopeEnd)));
+				} else {
+					throw std::runtime_error("Could not find matching ']' for '[': " + pattern);
+					return expVec;
+				}
+				
+				scopeStart = scopeEnd;
+				break;
+				
+			case '(': // Not implimented.
+				
+				scopeEnd = scopeStart + 1;
+				
+				if (findScope(scopeStart, scopeEnd, pattern, '(', ')')){
+						expVec.push_back(Expression(std::string(scopeStart, scopeEnd)));
+				} else {
+					throw std::runtime_error("Could not find matching ')' for '(': " + pattern);
+					return expVec;
+				}
+				
+				scopeStart = scopeEnd;
+				break;
+		
+			default:
+			
+				scopeEnd = scopeStart + 1;
+				expVec.push_back(Expression(std::string(scopeStart, scopeEnd)));
+				scopeStart = scopeEnd;
+				break;
+		}	
 	}
 	
 	return expVec;
+}
+
+bool regEx::findScope(std::string::const_iterator& scopeStart, std::string::const_iterator& scopeEnd, const std::string& pattern, const char& startChar, const char& endChar){
+	int scopeDepth = 0;
+	
+	while(scopeEnd != pattern.cend()){
+		if(*scopeEnd == endChar && scopeDepth == 0){
+			++scopeEnd; //scopeEnd needs to point to one after the final character.
+			std::cout << "Scope found: " << std::string(scopeStart, scopeEnd) << std::endl;
+			return true;
+		}
+		
+		if(*scopeEnd == startChar){
+			++scopeDepth;
+		} else if(*scopeEnd == endChar){
+			--scopeDepth;
+		}
+
+		++scopeEnd;
+		
+	}
+	
+	return false;
+}
+	
+
+
+bool regEx::matchExpression(std::vector<Expression>::const_iterator& exp, const std::vector<Expression>::const_iterator& end, std::string::const_iterator& match){
+	if (exp + 1 != end){
+		switch((*(exp + 1)).type){
+			
+			case Expression::MATCH_ONE_OR_MORE:
+				return postfixOneOrMore(exp, end, match);
+				
+			case Expression::MATCH_ZERO_OR_ONE:
+				return postfixZeroOrOne(exp, end, match);
+			
+			default:
+				break;
+		}
+	} 
+	
+	
+	switch((*exp).type){
+		case Expression::EXACT:
+			return (*exp).typeString[0] == *match;
+			
+		case Expression::DIGIT:
+			return std::isdigit(*match);
+			
+		case Expression::WORD:
+			return std::isalnum(*match) || *match == '_';
+			
+		case Expression::ANY:
+			return (*match) != '\0';
+			
+		case Expression::ANY_OF:
+			return matchAnyOf(*exp, match);
+			
+		case Expression::SCOPE:
+			return matchScope(*exp, match);
+			
+		case Expression::ANCHOR_END:
+			return exp + 1 == end;
+			
+		default:
+			return false;
+		
+	}
+	
+}
+
+bool regEx::matchHere(std::vector<Expression>::const_iterator& exp, const std::vector<Expression>::const_iterator& end, std::string::const_iterator& match){
+	std::string::const_iterator textCurr = match;
+	
+	while(*textCurr != '\0' && exp != end){
+		
+		std::cout << "Matching expression: " << (*exp).typeString << " (" << (*exp).type << ")\n"
+			<< "with starting character: " << *textCurr << std::endl;
+		
+		if (matchExpression(exp, end, textCurr)){
+			
+			std::cout << "Match. Continueing..." << std::endl;
+			
+			++exp;
+			++textCurr;
+			continue;
+		}
+		break;
+	}
+	
+	if(exp == end){ //If end of scope was reached, success. All match.
+	
+		std::cout << "Full match. Match complete." << std::endl;
+		
+		match = textCurr;
+		return true;
+	}
+	
+	std::cout << "No match. Aborting..." << std::endl;
+	
+	return false;
+}
+
+bool regEx::matchPattern(const std::string& pattern, const std::string& text){
+	const std::vector<Expression> expressions = parsePattern(pattern);
+	std::vector<Expression>::const_iterator exp = expressions.cbegin();
+	std::vector<Expression>::const_iterator end = expressions.cend();
+	
+	std::string::const_iterator textBegin = text.begin();
+	std::string::const_iterator match = text.begin();
+	
+	
+	if((*exp).type == Expression::ANCHOR_START)
+		return matchHere(exp, end, match);
+	
+	
+	while(*textBegin!= '\0'){
+	if (matchHere(exp, end, match))
+		return true;
+	
+	if(*textBegin == '\0')
+		return false;
+	
+	++textBegin;
+	match = textBegin;
+	exp = expressions.cbegin();
+	}
+	
+	return false;
+}
+
+
+// Specialty match functions:
+
+bool regEx::matchAnyOf(const Expression& group, std::string::const_iterator match){
+	// Starting index 1: directly after opening bracket. Length of size() - 2: length of full string excluding brackets.
+	std::vector<Expression> possibleMatches = parsePattern(group.typeString.substr(1, group.typeString.size() - 2));
+	
+	for(std::vector<Expression>::const_iterator exp = possibleMatches.cbegin();
+	exp != possibleMatches.cend(); ++exp){
+		if(matchExpression(exp, possibleMatches.cend(), match)){
+			return true;
+		}
+	}
+	return false;
+}
+
+bool regEx::matchScope(const Expression& scope, std::string::const_iterator match){
+	// Starting index 1: directly after opening bracket. Length of size() - 2: length of full string excluding parenthesis.
+	std::vector<Expression> fullScope = parsePattern(scope.typeString.substr(1, scope.typeString.size() - 2));
+	std::vector<std::vector<Expression>::const_iterator> subScopes = {fullScope.begin()};
+	
+	for(std::vector<Expression>::const_iterator exp = fullScope.cbegin();
+	exp != fullScope.cend(); ++exp){
+		if((*exp).type == Expression::OR){ // Could be refactored to more easily tell if a type is a conditional.
+			subScopes.push_back(exp);
+		}
+	}
+	
+	subScopes.push_back(fullScope.cend());
+	
+	
+	// Does not function with future possible '&' conditional operator. If implimented, refactor.
+	
+	int index = 0;
+	std::vector<Expression>::const_iterator currStart = subScopes[index];
+	
+	std::vector<Expression>::const_iterator currEnd;
+	
+	while(currStart != fullScope.cend()){
+		currEnd = subScopes[index + 1];
+		
+		if (matchHere(currStart, currEnd, match)){
+			return true;
+		}
+		
+		++index;
+		currStart = currEnd + 1;
+		
+	}
+	return false;
+}
+
+
+// Postfix match functions:
+
+bool regEx::postfixOneOrMore(std::vector<Expression>::const_iterator& exp, const std::vector<Expression>::const_iterator& end, std::string::const_iterator& match){
+	
+	std::vector<Expression>::const_iterator nextExp = exp + 2;
+	std::string::const_iterator currMatch = match;
+	
+	while(*currMatch != '\0' && matchExpression(exp, exp + 1, currMatch)){
+		++currMatch;
+	}
+	
+	while(currMatch != match){ // At least one functional match found.
+		--currMatch;
+		
+		
+		
+		if (matchHere(nextExp, end, currMatch)){
+			match = currMatch;
+			exp = end - 1;
+			return true;
+		}
+	}
+	
+	return false;
+	
+	// God, this is so funky.I didn't want to use heavy recursion, but I just couldn't find an effective way around it with this one.
+}
+
+bool regEx::postfixZeroOrOne(std::vector<Expression>::const_iterator& exp, const std::vector<Expression>::const_iterator& end, std::string::const_iterator& match){
+	
+	std::vector<Expression>::const_iterator nextExp = exp + 2;
+	std::string::const_iterator currMatch = match;
+	
+	
+	if(matchHere(nextExp, end, currMatch)){
+			match = currMatch;
+			exp = end - 1;
+			return true;
+	}
+	
+	if(matchExpression(exp, exp + 1, currMatch)){
+		
+		++currMatch;
+		
+		if (matchHere(nextExp, end, currMatch)){
+			match = currMatch;
+			exp = end - 1;
+			return true;
+		}
+	}
+	
+	return false;
+	
 }
--- src/regEx.h
+++ src/regEx.h
@@ -3,7 +3,8 @@
 #include <string>
 #include <vector>
 
-
+namespace regEx{
+	
 class Expression {
 public:
 	explicit Expression(std::string expString);
@@ -14,11 +15,9 @@ public:
 		DIGIT,
 		WORD,
 		ANY,
-		GROUP_START,
-		GROUP_END,
-		EITHER_OR_START,
-		EITHER_OR_MIDDLE,
-		EITHER_OR_END,
+		ANY_OF,
+		SCOPE,
+		OR,
 		ANCHOR_START,
 		ANCHOR_END,
 		MATCH_ONE_OR_MORE,
@@ -39,4 +38,26 @@ public:
 };
 
 
+
+
+
+
 std::vector<Expression> parsePattern(const std::string& pattern);
+bool findScope(std::string::const_iterator& scopeStart, std::string::const_iterator& scopeEnd, const std::string& pattern, const char& startChar, const char& endChar);
+
+bool matchExpression(std::vector<Expression>::const_iterator& exp, const std::vector<Expression>::const_iterator& end, std::string::const_iterator& match);
+bool matchPattern(const std::string& pattern, const std::string& text);
+bool matchHere(std::vector<Expression>::const_iterator& exp, const std::vector<Expression>::const_iterator& end, std::string::const_iterator& match);
+bool matchAnyOf(const Expression& group, std::string::const_iterator match);
+bool matchScope(const Expression& scope, std::string::const_iterator match);
+
+//bool conditionAND(const std::vector<Expression>& scope, const std::vector<Expression>::const_iterator& andPos);
+//bool conditionOR(const std::vector<Expression>& scope, const std::vector<Expression>::const_iterator& orPos);
+//bool conditionXOR(const std::vector<Expression>& scope, const std::vector<Expression>::const_iterator& xorPos);
+
+bool postfixOneOrMore(std::vector<Expression>::const_iterator& exp, const std::vector<Expression>::const_iterator& end, std::string::const_iterator& match);
+bool postfixZeroOrOne(std::vector<Expression>::const_iterator& exp, const std::vector<Expression>::const_iterator& end, std::string::const_iterator& match);
+//bool postfixZeroOrMore(std::vector<Expression>::const_iterator& exp, const std::vector<Expression>::const_iterator& end, std::string::const_iterator& match);
+
+
+} // regEx
